9b33ae81e393c0b0cd1ca1a73fe236ce48137a18
Lef Filippakis <lefphilip@live.com>
2019-05-06 23:20:10 +0200

    updated architecture + animation-game changes

--- agent.js
+++ agent.js
@@ -1,8 +1,10 @@
 // map actions to integers (i.e. array indices)
 let mapping = [ "p", "a", "s", "d", "f"];
+const numOfMoves = 5;
+const numOfDims = 2; // For gaussian random
 
 // random seq generator
-function randSeqGen(seqLen=10) {
+function randSeqGen(seqLen) {
     let output = [];
     for (let i = 0; i < seqLen; i++ ){
         output.push(Math.floor(Math.random() * mapping.length));
@@ -20,10 +22,16 @@ function randLabGen(seqLen=5) {
     return output;
 };
 
+function randGaussian(numOfDims) {
+    return tf.randomNormal([1, numOfDims]);
+}
+
 // Define the topology of the model.
 const model = tf.sequential();
-model.add(tf.layers.lstm({units: 8, inputShape: [10, 1]}));
-model.add(tf.layers.dense({units: 5, activation: 'sigmoid'}));
+model.add(tf.layers.dense({units: Math.ceil(numOfMoves), inputShape: [numOfDims], activation: 'relu'}));
+model.add(tf.layers.dense({units: Math.ceil(numOfMoves*5), activation: 'relu'}));
+model.add(tf.layers.dense({units: Math.ceil(numOfMoves*10), activation: 'relu'}));
+model.add(tf.layers.dense({units: numOfMoves*mapping.length, activation: 'sigmoid'}));
 model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
 
 // Compile model to prepare for training.
@@ -36,44 +44,49 @@ model.compile({
 });
 
 // get all the layers of the model
-const layers = model.layers
+const layers = model.layers;
+const numOfLayers = layers.length;
+const moveLayer = numOfLayers - 1;
 
 function layerOutputs(topInput) {
-    let input = [layers[0].apply(topInput)];
+    let input = [topInput];
     
     // passing Input to every layer and saving output as input for next layer    
-    for (var i = 1; i < model.layers.length; i++) {
-        input.push(layers[i].apply(input[i-1]));
+    for (var i = 0; i < model.layers.length; i++) {
+        input.push(layers[i].apply(input[i]));
+        // console.log('Name:' ,layers[i].name);
+        // console.log('Units:' ,layers[i].units);
+        // console.log('Out Shape:' ,layers[i].outputShape);
     };
     return input;
 }
 
-model.predict(tf.tensor([0,1,2,3,4,0,1,2,3,4], [1, 10, 1], "int32")).print();
 
-let outs = layerOutputs(tf.tensor([0,1,2,3,4,0,1,2,3,4], [1, 10, 1], "int32"));
-console.log(outs[1].dataSync());
+let initialSeed = randGaussian(numOfDims);
+model.predict(initialSeed).print();
 
-function computeNextMove(outs, ins) {
-    let maxInd = tf.tensor1d(outs).argMax().dataSync();
-    return ins.concat(tf.tensor1d(maxInd));
+let outs = layerOutputs(initialSeed);
+console.log(outs[moveLayer].dataSync());
+
+function extractMoves(outs) {
+    return tf.tensor2d(outs, [numOfMoves, mapping.length]).argMax(axis=1);
 }
 
 let trainSet = [];
 for (let i = 0; i < 500; i++){
-    trainSet.push(randSeqGen());
+    trainSet.push(randGaussian(numOfDims));
 };
 
 let trainIndex = 0;
-let newMoves = computeNextMove(outs[1].dataSync(), tf.tensor1d(trainSet[trainIndex]));
+let newMoves = extractMoves(outs[moveLayer].dataSync());
 
 function trainIteration(train, label) {
     console.log('Training model...');
-    let input = tf.tensor(train, [1, train.length, 1]);
-    input.print();
+    train.print();
     let action = tf.tensor(label, [label.length, 1]);
     action.print();
     model.fit(
-        input,
+        train,
         action, 
         {
             epochs: 1,
@@ -88,8 +101,7 @@ function trainIteration(train, label) {
         }
     );
     trainIndex += 1;
-    let newMove = tf.tensor(trainSet[trainIndex], [1, trainSet[trainIndex].length, 1]);
-    newMoves = computeNextMove(layerOutputs(newMove)[1].dataSync(), tf.tensor1d(trainSet[trainIndex]))
+    newMoves = extractMoves(layerOutputs(trainSet[trainIndex])[moveLayer].dataSync());
 };
 
 setInterval(function() {playAnimation()}, 5000);
@@ -101,7 +113,7 @@ function playAnimation() {
         let data = {"key": mapping[animSeq[i]]}
         keyAction(data);
         if (++i < animSeq.length) {
-            setTimeout(loop, 250);  // call myself in 3 seconds time if required
+            setTimeout(loop, 100);
         }
     })();
 };
--- gameEngine.js
+++ gameEngine.js
@@ -30,12 +30,12 @@ function degToRad(deg) {
 var ground = Bodies.rectangle(400, 510, 810, 60, { isStatic: true });
 
 // create dog
-var body = Bodies.rectangle(300, 50, 100, 20);
-var head = Bodies.rectangle(350, 30, 50, 20, { angle: degToRad(-45) });
+var body = Bodies.rectangle(300, 50, 100, 20, {label: 'body'});
+var head = Bodies.rectangle(350, 30, 50, 20, { angle: degToRad(-45), label: 'head' });
 // Body.rotate(head, -120);
-var backLeg = Bodies.rectangle(250, 60, 50, 20, { angle: degToRad(-45) });
+var backLeg = Bodies.rectangle(250, 60, 50, 20, { angle: degToRad(-90), label: 'backLeg' });
 // Body.rotate(backLeg, -130);
-var frontLeg = Bodies.rectangle(350, 60, 50, 20, { angle: degToRad(45) });
+var frontLeg = Bodies.rectangle(350, 60, 50, 20, { angle: degToRad(90), label: 'frontLeg' });
 // Body.rotate(frontLeg, 130);
 
 var dog = Body.create({
@@ -74,6 +74,12 @@ function timeElapsedKey() {
     return timeElapsed;
 }
 
+function resetBody(){
+    Body.setAngle(backLeg, degToRad(-90));
+    Body.setAngle(frontLeg, degToRad(90));
+    Body.setPosition(dog, Matter.Vector.create(250, 400));
+};
+
 let goodDances = [];
 
 function keyAction(e) {
@@ -86,15 +92,17 @@ function keyAction(e) {
     } else if (e.key == "s") {
         Body.rotate(backLeg, degToRad(45));
     } else if (e.key == " ") {
-        Body.setPosition(dog, Matter.Vector.create(250, 0));
+        resetBody();
     } else if (e.key == "b") {
         trainIteration(trainSet[trainIndex], [0]);
+        resetBody();
     } else if (e.key == "g") {
         goodDances.push(trainSet[trainIndex]);
         trainIteration(trainSet[trainIndex], [1]);
+        resetBody();
     }
 
-    // update timer if know key was pressed
+    // update timer if known key was pressed
     if (mapping.includes(e.key)) {
         timeElapsedKey();
     }
